7098d808628400805df0557c720932c589138e93
Peter <petermada@gmail.com>
2022-02-28 11:50:30 +0100

    Create DB connection

---
+++ server/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'wert1189',
+  host: 'localhost',
+  port: 5432, //automatic run on this port
+  database: 'ABADataEvaluation',
+});
+
+module.exports = pool;

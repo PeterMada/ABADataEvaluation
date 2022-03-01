b25980184417123079b24f832d08cacd4fdfc6c5
Peter <petermada@gmail.com>
2022-03-01 15:01:27 +0100

    Basic dashboard

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 
 //register and login
 app.use('/auth', require('./routes/jwtAuth'));
+app.use('/dashobard', require('./routes/dashboard'));
 
 app.listen(5000, () => {
   console.log(`Server is starting on port 5000`);

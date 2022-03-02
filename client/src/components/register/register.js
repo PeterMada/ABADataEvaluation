8fd2d069d389453c716c6b9b8ecfd4b5abe95497
Peter <petermada@gmail.com>
2022-03-02 10:03:24 +0100

    Register process

--- client/src/components/register/register.js
+++ client/src/components/register/register.js
@@ -1,9 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const Register = () => {
+export const Register = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+    name: '',
+  });
+
+  const { email, password, name } = inputs;
+
+  const onChange = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { email, password, name };
+      console.log(JSON.stringify(body));
+      const response = await fetch('http://localhost:5000/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+
+        body: JSON.stringify(body),
+      });
+
+      const parseRes = await response.json();
+
+      //TODO store token in cookie
+      localStorage.setItem('token', parseRes.token);
+      setAuth(true);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <>
       <h1>Register</h1>
+      <form onSubmit={onSubmitForm}>
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="name">
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => onChange(e)}
+          />
+        </div>{' '}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+            type="password"
+            placeholder="******************"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+          <p className="text-red-500 text-xs italic">
+            Please choose a password.
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit">
+            Register
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#">
+            Forgot Password?
+          </a>
+        </div>
+      </form>
     </>
   );
 };

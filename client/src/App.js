74880a29f8190c5a8b4c64e0bb84ee88b754b506
Peter <petermada@gmail.com>
2022-05-18 19:24:31 +0200

    Remove login

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import { Header } from './layout/header/Header';
 import { Dashboard } from './screen/Dashboard';
-import { Login } from './components/login/Login';
+//import { Login } from './components/login/Login';
 import { Register } from './components/register/register';
 import { Profile } from './components/profile/profile';
 import { PersonForm } from './forms/personForm/PersonForm';
@@ -338,6 +338,26 @@ export const App = () => {
               )
             }
           />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+/*
+<Route
+  exact
+  path="/"
+  index
+  element={
+    !isAuthenticated ? (
+      <Login setAuth={setAuth} />
+    ) : (
+      <Navigate replace to="/dashboard" />
+    )
+  }
+/>;
+
 
           <Route
             exact
@@ -362,23 +382,4 @@ export const App = () => {
               )
             }
           />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-};
-
-/*
-<Route
-  exact
-  path="/"
-  index
-  element={
-    !isAuthenticated ? (
-      <Login setAuth={setAuth} />
-    ) : (
-      <Navigate replace to="/dashboard" />
-    )
-  }
-/>;
 */
